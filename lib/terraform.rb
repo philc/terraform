1d32dd098bbd2e73b6dd6ce3c76636a5e52db84c
Daniel MacDougall <dmacdougall@gmail.com>
2012-10-17 16:49:53 -0700

    Mark `Terraform.write_dsl_file` as deprecated

--- lib/terraform.rb
+++ lib/terraform.rb
@@ -2,8 +2,11 @@ require "terraform/version"
 require "fileutils"
 
 module Terraform
+  # DEPRECATED
+  # Replaced by `write_terraform_files`. Remove in next major version release.
   # Writes the terraform_dsl.rb to the given file or directory.
   def self.write_dsl_file(path)
+    puts "WARNING: `Terraform.write_dsl_file` is deprecated. Instead use `Terraform.write_terraform_files`."
     path = File.join(path, "terraform_dsl.rb") if File.directory?(path)
     FileUtils.cp(File.expand_path(File.join(File.dirname(__FILE__), "terraform/dsl.rb")), path)
   end
